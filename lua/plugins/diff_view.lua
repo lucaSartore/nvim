6513c2d08f3eb09700c32441b3b121e8b4744dd6
Luca Sartore <lucasar.com8@gmail.com>
2024-10-02 11:49:56 +0200

    Add documentation command

--- lua/plugins/diff_view.lua
+++ lua/plugins/diff_view.lua
@@ -2,6 +2,9 @@ return {
     "sindrets/diffview.nvim",
     config = function ()
 		vim.api.nvim_set_keymap("n", "<leader>gd", ":DiffviewOpen<cr>", { desc = "[G]ithub [D]iff" })
+        -- Examples of commands 
+        -- :DiffviewOpen HEAD~4..HEAD~2
+        -- :DiffviewOpen d4a7b0d
 		vim.api.nvim_set_keymap("n", "<leader>gD", ":DiffviewOpen", { desc = "[G]ithub [D]iff with options" })
     end
 }
