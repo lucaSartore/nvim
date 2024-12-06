026224499e9a7d545078c33654e266c3b0fc3cb6
lucaSartore <lucasar.com8@gmail.com>
2024-12-06 08:34:44 +0100

    Add configuration to use powershell in lazygit/toggleterm

--- init.lua
+++ init.lua
@@ -122,7 +122,19 @@ vim.api.nvim_set_keymap('v', '<leader>p', '"ap', { noremap = true, silent = true
 vim.opt.whichwrap = "b,s,h,l"
 
 -- using powershell as default terminal
-vim.opt.shell='powershell.exe'
+-- Some of the options are required to make powershell work with toggleterm/lazygit
+-- credit: https://github.com/akinsho/toggleterm.nvim/wiki/Tips-and-Tricks#using-toggleterm-with-powershell
+local powershell_options = {
+  shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+  shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+  shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+  shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+  shellquote = "",
+  shellxquote = "",
+}
+for option, value in pairs(powershell_options) do
+  vim.opt[option] = value
+end
 vim.cmd('set shellcmdflag="-c"')
 
 require("config.lazy")
