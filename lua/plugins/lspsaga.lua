e4b83a14937607045357e7bedd67ccf0f8432860
lucaSartore <lucasar.com8@gmail.com>
2025-02-01 09:42:55 +0100

    Add goto/peack type definition

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -6,5 +6,7 @@ return {
         local saga = require('lspsaga')
         saga.setup({})
         vim.api.nvim_set_keymap('n', 'gh', '<cmd>Lspsaga hover_doc<CR>', { silent = true, noremap = true })
+        vim.api.nvim_set_keymap('n', '<leader>pt', '<cmd>Lspsaga peek_type_definition<CR>', { silent = true, noremap = true })
+        vim.api.nvim_set_keymap('n', '<leader>gt', '<cmd>Lspsaga goto_type_definition<CR>', { silent = true, noremap = true })
 	end,
 }
