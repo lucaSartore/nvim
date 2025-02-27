c1ee052ecd071eb94f288ae701419560ef1d7fff
lucaSartore <lucasar.com8@gmail.com>
2025-02-27 10:10:49 +0100

    Add fastspell.nvim plugin

---
+++ lua/plugins/fastspell.lua
@@ -0,0 +1,19 @@
+return {
+	"lucaSartore/fastspell.nvim",
+	config = function()
+		local fastspell = require("fastspell")
+
+		fastspell.setup()
+
+		vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI", "BufEnter", "WinScrolled" }, {
+			callback = function(_)
+				local first_line = vim.fn.line("w0")
+				local last_line = vim.fn.line("w$")
+				fastspell.sendSpellCheckRequest(first_line, last_line)
+
+				-- local end_line = vim.api.nvim_buf_line_count(0)
+				-- spell_check_request.sendSpellCheckRequest(0, end_line)
+			end,
+		})
+	end,
+}
