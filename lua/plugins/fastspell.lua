1383d231ef5cb78000d885a4adeb9912b576389c
lucaSartore <lucasar.com8@gmail.com>
2025-03-05 18:37:40 +0100

    only update spelling in proximity to current cursor position

--- lua/plugins/fastspell.lua
+++ lua/plugins/fastspell.lua
@@ -5,10 +5,11 @@ return {
 
 		fastspell.setup()
 
-		vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI", "BufEnter", "WinScrolled" }, {
+		vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI"}, {
 			callback = function(_)
-                local first_line = vim.fn.line('w0')-1
-                local last_line = vim.fn.line('w$')
+                local current_row = vim.api.nvim_win_get_cursor(vim.api.nvim_get_current_win())[1]
+                local first_line = current_row-2
+                local last_line = current_row+1
                 fastspell.sendSpellCheckRequest(first_line, last_line)
 			end,
 		})
