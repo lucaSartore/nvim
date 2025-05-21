794b8db7ce5455d005a29277028b9cb63301d6c3
lucaSartore <lucasar.com8@gmail.com>
2025-05-21 19:13:50 +0200

    update pyenv

--- lua/plugins/python_venv_selector.lua
+++ lua/plugins/python_venv_selector.lua
@@ -1,17 +1,32 @@
-return {
-  'linux-cultist/venv-selector.nvim',
-  dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
-  opts = {
-    anaconda_envs_path = 'C:\\Users\\lucas\\.conda\\envs',
-    anaconda = {
-        python_parent_dir = ".\\"
+
+local search
+if vim.fn.has('win32') == 1 then
+    search = {
+        myconda = {
+            command = "$FD python.exe$ C:/tools/miniconda3/envs --no-ignore-vcs --full-path -a -E Lib",
+        },
+        myconda2 = {
+            command = "$FD python.exe$ C:/Users/lucas/.conda/envs --no-ignore-vcs --full-path -a -E Lib",
+        },
+        all_envs_in_path = {
+            command = "where python"
+        }
     }
+else
+    search = {}
+end
+
+
+return {
+  "linux-cultist/venv-selector.nvim",
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
+    { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
   },
-  event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
-  keys = {
-    -- Keymap to open VenvSelector to pick a venv.
-    { '<leader>vs', '<cmd>VenvSelect<cr>' },
-    -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-    { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
+  lazy = false,
+  branch = "regexp",
+  opts = {
+        search = search
   },
 }
