04c94344e36b07f010a202c83f0a334b393d14fd
Luca Sartore <lucasartore02@gmail.com>
2025-10-13 12:18:07 +0200

    Add reminder for C# formatter

--- lua/language_tools/formatter/csharp.lua
+++ lua/language_tools/formatter/csharp.lua
@@ -1,6 +1,9 @@
 -- CSharp formatter configuration
 local M = {}
 
+-- if not installed using nix use:
+-- dotnet tool install -g csharpier --version 0.30.6
+
 local nix_installation = vim.fn.executable("dotnet-csharpier") ~= 0
 local command = nix_installation  and "dotnet-csharpier" or "csharpier"
 
