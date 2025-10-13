6f2d8dbccdef1b2756fa392c0341f1b1a275ca9e
Luca Sartore <lucasartore02@gmail.com>
2025-10-13 12:08:28 +0200

    Add command detection for c# formatter

--- lua/language_tools/formatter/csharp.lua
+++ lua/language_tools/formatter/csharp.lua
@@ -1,10 +1,13 @@
 -- CSharp formatter configuration
 local M = {}
 
+local nix_installation = vim.fn.executable("dotnet-csharpier") ~= 0
+local command = nix_installation  and "dotnet-csharpier" or "csharpier"
+
 M.config = {
     function()
         return {
-            exe = "dotnet-csharpier",
+            exe = command,
             stdin = true,
         }
     end
