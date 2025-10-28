360fd33ac73ca2ae57c69ee6f24055253ff09135
Luca Sartore <lucasartore02@gmail.com>
2025-10-28 12:45:07 +0100

    update csharpier command

--- lua/language_tools/formatter/csharp.lua
+++ lua/language_tools/formatter/csharp.lua
@@ -2,18 +2,18 @@
 local M = {}
 
 -- if not installed using nix use:
--- dotnet tool install -g csharpier --version 0.30.6
-
-local nix_installation = vim.fn.executable("dotnet-csharpier") ~= 0
-local command = nix_installation  and "dotnet-csharpier" or "csharpier"
+-- dotnet tool install -g csharpier
 
 M.config = {
-    function()
-        return {
-            exe = command,
-            stdin = true,
-        }
-    end
+	function()
+		return {
+			exe = "csharpier",
+			args = {
+				"format",
+			},
+			stdin = true,
+		}
+	end,
 }
 
 return M
