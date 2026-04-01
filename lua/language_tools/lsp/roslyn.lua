a7d77f0e0e96db20b3d909a264850d78a513d93b
Luca Sartore <lucasartore02@gmail.com>
2026-04-01 16:48:39 +0200

    fix foslyn solution

--- lua/language_tools/lsp/roslyn.lua
+++ lua/language_tools/lsp/roslyn.lua
@@ -3,12 +3,12 @@ local M = {}
 
 ---@param capabilities table LSP capabilities
 function M.setup(capabilities)
-    vim.lsp.config("roslyn-ls",{
+    vim.lsp.config("roslyn_ls",{
         capabilities = capabilities,
         settings = {
         }
     })
-    vim.lsp.enable("roslyn-ls")
+    vim.lsp.enable("roslyn_ls")
 end
 
 return M
